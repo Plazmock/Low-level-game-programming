84235f4120832af7c4cfcaf3058ea306b1806683
samir-github <s.muradov88@gmail.com>
2015-10-13 17:50:31 +0300

    Testing github

--- Win32Project1/handmade.cpp
+++ Win32Project1/handmade.cpp
@@ -25,18 +25,143 @@ global_variable int BitmapHeight;
 global_variable int BytesPerPixel = 4;
 global_variable bool running = true;
 
-//	Function declarations
-//	The function that handles messages to the window
-LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
-//	Resize Divice Independent Bitmap
-internal void Win32ResizeDIBSelect(int width, int heigth);
-internal void Win32UpdateWindow(HDC DeviceContext, RECT *ClientRect, int x, int y, int width, int height);
-internal void RenderWierdGradient(int xOffset, int yOffset);
+// Render wierd colored moving boxes on the creen
+internal void RenderWierdGradient(int xOffset, int yOffset)
+{
+	int width = BitmapWidth;
+	int height = BitmapHeight;
+
+	int Pitch = width*BytesPerPixel;
+	uint8 *Row = (uint8 *)BitmapMemory;
+	for (int y = 0; y < BitmapHeight; ++y)
+	{
+		uint32 *Pixel = (uint32 *)Row;
+		for (int x = 0; x < BitmapWidth; ++x)
+		{
+			/*
+			Pixel in Memory:	BB GG RR xx
+			Pixel in Register:  xx RR GG BB
+			0xxxRRGGBB
+			*/
+			uint8 Blue = x + xOffset;
+			uint8 Green = y + yOffset;
+
+			*Pixel++ = ((Green << 8) | Blue);
+		}
+		Row += Pitch;
+	}
+}
+
+
+// Resize Divice Independent Bitmap
+// Fill the BitmapMemory that Win32UpdateWindow uses to PAINT on the window
+internal void Win32ResizeDIBSelect(int width, int height)
+{
+	// TODO: Booletproof this
+	// Maybe dont free first, free after, then free is that fails
+	if (BitmapMemory)
+	{
+		VirtualFree(BitmapMemory, 0, MEM_RELEASE);
+	}
+
+	BitmapWidth = width;
+	BitmapHeight = height;
+
+	BitmapInfo.bmiHeader.biSize = sizeof(BitmapInfo.bmiHeader);
+	BitmapInfo.bmiHeader.biWidth = BitmapWidth;
+	BitmapInfo.bmiHeader.biHeight = -BitmapHeight; // Negative Height makes the Bitmap a Topdown DIP with origin upper-left corrner
+	BitmapInfo.bmiHeader.biPlanes = 1;
+	BitmapInfo.bmiHeader.biBitCount = 32;
+	BitmapInfo.bmiHeader.biCompression = BI_RGB;
+
+	int BitmapMemorySize = BitmapWidth*BitmapHeight*BytesPerPixel;
+	BitmapMemory = VirtualAlloc(NULL, BitmapMemorySize, MEM_COMMIT, PAGE_READWRITE);
+
+	// TODO: Probably clear this to black
+
+}
+
+// Put the BitmapMemory on the screen
+internal void Win32UpdateWindow(HDC DeviceContext, RECT ClientRect, int x, int y, int width, int height)
+{
+	int WindowWidth = ClientRect.right - ClientRect.left;
+	int WindowHeight = ClientRect.bottom - ClientRect.top;
+	// BitBlt is better alternative?
+	StretchDIBits(DeviceContext,
+		/*
+		x, y, width, height,
+		x, y, width, height,
+		*/
+		0, 0, BitmapWidth, BitmapHeight,
+		0, 0, WindowWidth, WindowHeight,
+		BitmapMemory, &BitmapInfo,
+		DIB_RGB_COLORS, SRCCOPY);
+
+}
+
+
+// Main Windows Procedure function. Handles the messages recived from the operating system.
+LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	LRESULT Result = 0;
+
+	switch (uMsg)
+	{
+	case WM_PAINT: // Paint to the window
+	{
+		PAINTSTRUCT Paint;
+		HDC DeviceContext = BeginPaint(hWnd, &Paint);
+		int x = Paint.rcPaint.left;
+		int y = Paint.rcPaint.top;
+		int width = Paint.rcPaint.right - Paint.rcPaint.left;
+		int height = Paint.rcPaint.bottom - Paint.rcPaint.top;
+		// Clean up double ClientRect use in code?
+		RECT ClientRect;
+		GetClientRect(hWnd, &ClientRect);
+		Win32UpdateWindow(DeviceContext, ClientRect, x, y, width, height);
+		EndPaint(hWnd, &Paint);
+	} break;
+
+	case WM_SIZE:
+	{
+		RECT ClientRect;
+		GetClientRect(hWnd, &ClientRect);
+		int width = ClientRect.right - ClientRect.left;
+		int height = ClientRect.bottom - ClientRect.top;
+		Win32ResizeDIBSelect(width, height);
+
+
+	} break;
+
+	case WM_DESTROY:
+	{
+		PostQuitMessage(WM_QUIT); // Will end the while(GetMessage) loop witch will cause the main to finish.
+	} break;
+
+	case WM_CLOSE:
+	{
+		PostQuitMessage(WM_QUIT); // Will end the while(GetMessage) loop witch will cause the main to finish.
+	} break;
+
+	case WM_ACTIVATEAPP:
+	{
+		OutputDebugStringA("WM_ACTIVATEAPP\n");
+	} break;
+
+	default:
+	{
+		Result = DefWindowProc(hWnd, uMsg, wParam, lParam);
+	} break;
+
+	}
+
+	return Result;
+}
 
 //	Main.
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
-
+	
 //	The window class
 	WNDCLASS WindowClass = {};
 	WindowClass.style		   = CS_OWNDC | CS_HREDRAW | CS_VREDRAW;
@@ -67,9 +192,10 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 //			Listen for messagess and send them to be handeld 
 			int xOffset = 0;
 			int yOffset = 0;
-			MSG msg;
+
 			while (running)
 			{	
+				MSG msg;
 				while (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE)) // If Message = VM_QUIT, GetMeassage returns 0
 				{
 					if (msg.message == WM_QUIT)
@@ -87,11 +213,12 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 				GetClientRect(Window, &ClientRect);
 				int WindowWidth = ClientRect.right - ClientRect.left;
 				int WindowHeight = ClientRect.bottom - ClientRect.top;
-				Win32UpdateWindow(DeviceContext, &ClientRect, 0, 0, WindowWidth, WindowHeight);
+				Win32UpdateWindow(DeviceContext, ClientRect, 0, 0, WindowWidth, WindowHeight);
 				ReleaseDC(Window, DeviceContext);
 				
 
 				++xOffset;
+				yOffset++;
 			}
 		}
 		else
@@ -110,131 +237,3 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 	return 0;
 }
 
-// Main Windows Procedure function. Handles the messages recived from the operating system.
-LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
-{ 
-	LRESULT Result = 0;
-	
-	switch (uMsg)
-	{
-		case WM_PAINT: // Paint to the window
-		{
-			PAINTSTRUCT Paint;
-			HDC DeviceContext = BeginPaint(hWnd, &Paint);
-			int x = Paint.rcPaint.left;
-			int y = Paint.rcPaint.top;
-			int width = Paint.rcPaint.right - Paint.rcPaint.left;
-			int height = Paint.rcPaint.bottom - Paint.rcPaint.top;
-			// Clean up double ClientRect use in code?
-			RECT ClientRect;
-			GetClientRect(hWnd, &ClientRect);
-			Win32UpdateWindow(DeviceContext, &ClientRect, x, y, width, height);
-			EndPaint(hWnd, &Paint);
-		} break;
-
-		case WM_SIZE:
-		{
-			RECT ClientRect;
-			GetClientRect(hWnd, &ClientRect);
-			int width = ClientRect.right - ClientRect.left;
-			int height = ClientRect.bottom - ClientRect.top;
-			Win32ResizeDIBSelect(width, height);
-			
-
-		} break;
-
-		case WM_DESTROY:
-		{
-			PostQuitMessage(WM_QUIT); // Will end the while(GetMessage) loop witch will cause the main to finish.
-		} break;
-
-		case WM_CLOSE:
-		{
-			PostQuitMessage(WM_QUIT); // Will end the while(GetMessage) loop witch will cause the main to finish.
-		} break;
-
-		case WM_ACTIVATEAPP:
-		{
-			OutputDebugStringA("WM_ACTIVATEAPP\n");
-		} break;
-
-		default:
-		{
-			Result = DefWindowProc(hWnd, uMsg, wParam, lParam);
-		} break;
-		
-	}
-
-	return Result;
-}
-// Resize Divice Independent Bitmap
-// Fill the BitmapMemory that Win32UpdateWindow uses to PAINT on the window
-internal void Win32ResizeDIBSelect(int width, int height)
-{
-	// TODO: Booletproof this
-	// Maybe dont free first, free after, then free is that fails
-	if (BitmapMemory)
-	{
-		VirtualFree(BitmapMemory, 0, MEM_RELEASE);
-	}
-
-	BitmapWidth = width;
-	BitmapHeight = height;
-	
-	BitmapInfo.bmiHeader.biSize = sizeof(BitmapInfo.bmiHeader);
-	BitmapInfo.bmiHeader.biWidth = BitmapWidth;
-	BitmapInfo.bmiHeader.biHeight = -BitmapHeight; // Negative Height makes the Bitmap a Topdown DIP with origin upper-left corrner
-	BitmapInfo.bmiHeader.biPlanes = 1;
-	BitmapInfo.bmiHeader.biBitCount = 32;
-	BitmapInfo.bmiHeader.biCompression = BI_RGB;
-	
-	int BitmapMemorySize= BitmapWidth*BitmapHeight*BytesPerPixel;
-	BitmapMemory = VirtualAlloc(NULL, BitmapMemorySize, MEM_COMMIT, PAGE_READWRITE);
-
-	// TODO: Probably clear this to black
-
-}
-
-// Put the BitmapMemory on the screen
-internal void Win32UpdateWindow(HDC DeviceContext,RECT *ClientRect, int x, int y, int width, int height)
-{
-	int WindowWidth = ClientRect->right - ClientRect->left;
-	int WindowHeight = ClientRect->bottom - ClientRect->top;
-	// BitBlt is better alternative?
-	StretchDIBits(DeviceContext,
-					/*
-					x, y, width, height,
-					x, y, width, height,
-					*/
-					0, 0, BitmapWidth, BitmapHeight,
-					0, 0, WindowWidth, WindowHeight,
-					BitmapMemory, &BitmapInfo,
-					DIB_RGB_COLORS, SRCCOPY);
-
-}
-
-internal void RenderWierdGradient(int xOffset, int yOffset)
-{
-	int width = BitmapWidth;
-	int height = BitmapHeight;
-
-	int Pitch = width*BytesPerPixel;
-	uint8 *Row = (uint8 *)BitmapMemory;
-	for (int y = 0; y < BitmapHeight; ++y)
-	{
-		uint32 *Pixel = (uint32 *)Row;
-		for (int x = 0; x < BitmapWidth; ++x)
-		{
-			/*
-			Pixel in Memory:	BB GG RR xx
-			Pixel in Register:  xx RR GG BB
-			0xxxRRGGBB
-			*/
-			uint8 Blue = x + xOffset;
-			uint8 Green = y + yOffset;
-			
-			*Pixel++ = ((Green << 8) | Blue);
-		}
-		Row += Pitch;
-	}
-}
